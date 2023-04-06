da6a680e06c1aa40cede19469510fe33fe78c59a
Greice-Castilho <greice.castilho@fatec.sp.gov.br>
2023-04-06 19:52:37 -0300

    first commit

---
+++ src/test/java/br/gov/sp/fatec/projetospringestudo/ProjetoSpringEstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.projetospringestudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringEstudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
